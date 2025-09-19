2e0be68f9c0b3285130e423165ed0e8bfc7e101d
dofoe17 <155560295+dofoe17@users.noreply.github.com>
2025-09-19 16:36:56 +0100

    Add files via upload
    Aligned summary table with pie chart and added more conditions to categorise function

--- budgeting_v2.py
+++ budgeting_v2.py
@@ -9,14 +9,14 @@ def categorise(desc):
 
     if 'lidl' in desc or 'tesco' in desc or 'sainsbury' in desc or 'aldi' in desc: 
         return 'Groceries'
-    elif 'tfl travel' in desc or 'uber' in desc or 'trainline' in desc: 
-        return 'Travel'
     elif 'spotify' in desc or 'energie' in desc or 'vision' in desc: 
         return 'Subscriptions'
     elif 'with energy' in desc or 'virgin media' in desc: 
         return 'Bills'
-    elif 'deliveroo' in desc: 
+    elif 'deliveroo' in desc or 'uber eats' in desc: 
         return 'Take-Away'
+    elif 'tfl travel' in desc or 'uber' in desc or 'trainline' in desc: 
+        return 'Travel'
     elif 'pele barbers' in desc: 
         return 'Haircut'
     elif 'payment received - thank you' in desc: 
@@ -35,14 +35,13 @@ st.set_page_config(page_title='My Finance App', page_icon='💷', layout='wide')
 def load_transactions(file): 
     df = pd.read_csv(file)
     df.columns = [col.strip() for col in df.columns] #we want to strip any leading or preceding white spaces from data column headers
-
-    #st.write(df) #This shows the full DF 
     return df
-
+    
 
 def main():
     st.title('Budgeting App')
     uploaded_file = st.file_uploader('Upload transaction CSV file', type=['csv'])
+
     if uploaded_file is not None: #if upload file exists 
         df = load_transactions(uploaded_file)
         
@@ -63,28 +62,31 @@ def main():
                 category_totals = category_totals.sort_values(by='Amount', ascending=False)
                 expenses_over_time = debits_df.groupby('Date')['Amount'].sum().reset_index() 
                 expenses_over_time = expenses_over_time.sort_values(by='Date', ascending=False)
+                
+                col1, col2, col3 = st.columns([1, 0.5, 1])
+                with col1:
+                    #Pie chart visualisation 
+                    fig = px.pie(
+                        category_totals,
+                        values= 'Amount',
+                        names= 'Category', 
+                        title= 'Expense by Category'
+                    )
+                    st.plotly_chart(fig, use_container_width=True)
 
-                st.dataframe(
-                    category_totals, 
-                    column_config={'Amount': st.column_config.NumberColumn('Amount', format='%.2f GBP')}, 
-                    use_container_width=True, #make table fit width of screen
-                    hide_index=True
-                )
-                #Add expense total figure
-                debit_totals = debits_df['Amount'].sum()
-                st.metric('Total Expenses', f'£{debit_totals:,.2f}')
-
-                #Pie chart visualisation 
-                fig = px.pie(
-                    category_totals,
-                    values= 'Amount',
-                    names= 'Category', 
-                    title= 'Expense by Category'
-                )
-                st.plotly_chart(fig, use_container_width=True)
+                with col3: 
+                    st.dataframe(
+                        category_totals, 
+                        column_config={'Amount': st.column_config.NumberColumn('Amount', format='%.2f GBP')}, 
+                        use_container_width=True, #make table fit width of screen
+                        hide_index=True
+                    )
+                    #Add expense total figure
+                    debit_totals = debits_df['Amount'].sum()
+                    st.metric('Total Expenses', f'£{debit_totals:,.2f}')
 
                 #Line chart visualisation
-                fig = px.line(
+                fig = px.area(
                     expenses_over_time, 
                     x='Date', 
                     y='Amount',
